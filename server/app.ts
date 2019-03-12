a90267440cab072a168dc8904b7c632dc5613186
lanjz <lanjz@mingyuanyun.com>
2019-03-12 11:01:55 +0800

    测试

--- server/app.ts
+++ server/app.ts
@@ -21,6 +21,6 @@ if(!process.env.JUSTNODE) {
 middleware(app)
 // mon()
 
-app.listen(3001, () => {
-  console.log('Example app listening on port 3001!\n')
+app.listen(3002, () => {
+  console.log('Example app listening on port 3002!\n')
 })
