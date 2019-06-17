44b03e4cfff8b0783a5c9bdbd9b417ca0cb306ac
lanjz <lanjz@mingyuanyun.com>
2019-06-17 10:59:22 +0800

    feat:跨域设置

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,3 +1,4 @@
+const cors = require('koa2-cors')
 const bodyParser = require('koa-bodyparser')
 const router  = require( '../router')
 const sent  = require( '../utils/ret')
@@ -5,6 +6,20 @@ const hello  = require( '../utils/hello.js')
 const checkAuth = require( '../utils/checkAuth.js')
 
 module.exports = function (app) {
+  app.use(cors({
+    origin: function (ctx) {
+      if (ctx.method.toLowerCase() === 'get' ||
+        (ctx.method.toLowerCase() === 'post' && ctx.url === '/login')) {
+        return "http://localhost:3002"; // 允许来自所有域名请求
+      }
+      return 'http://localhost:3002';
+    },
+    allowMethods: ['GET', 'POST'],
+/*  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],*/
+  }))
   app.use(bodyParser())
   app.use(sent())
   app.use(hello.errorHandle)
