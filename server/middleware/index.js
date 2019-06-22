8ab79fd525b4a0878a587b9201a21b66b4e0da78
lanjz <lanjz@mingyuanyun.com>
2019-06-22 14:24:57 +0800

    feat:解决本地跨域时，set-cookie失败问题

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -9,24 +9,26 @@ const host = process.env.NODE_ENV === 'development' ? '*' : 'localhost:3002'
 module.exports = function (app) {
 /*  app.use(cors({
     origin: function (ctx) {
-      if (ctx.method.toLowerCase() === 'get' ||
+ /!*     if (ctx.method.toLowerCase() === 'get' ||
         (ctx.method.toLowerCase() === 'post' && ctx.url === '/login')) {
         return "http://localhost:3002"; // 允许来自所有域名请求
-      }
-      return 'http://localhost:3002';
+      }*!/
+      return '*';s
     },
-    allowMethods: ['GET', 'POST'],
-/!*  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    allowMethods: ['GET', 'POST', 'options'],
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
     maxAge: 5,
     credentials: true,
-    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],*!/
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
   }))*/
   app.use(async function(ctx, next) {
-    ctx.set("Access-Control-Allow-Methods", "*");
-    ctx.set("Access-Control-Allow-Origin", host)
-    ctx.set("Access-Control-Allow-Credentials", true);
-    ctx.set("Access-Control-Max-Age", 86400000);
-    ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type");
+    if(process.env.NODE_ENV === 'development') {
+      ctx.set("Access-Control-Allow-Methods", "*");
+      ctx.set("Access-Control-Allow-Origin", 'http://localhost:3002')
+      ctx.set("Access-Control-Allow-Credentials", true);
+      ctx.set("Access-Control-Max-Age", 86400000);
+      ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type");
+    }
     await next()
   })
   app.use(bodyParser())
